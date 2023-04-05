af097d5cb220e9458d637e87e26b7dc1337d897a
Marc13 <83296140+llamaair@users.noreply.github.com>
2023-04-05 13:58:13 +0200

    Create stations.php

---
+++ eddb2/stations.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>EDDB</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>EDDB</h1>
+        <nav>
+            <ul>
+                <li><a href="/eddb2/systems.php">Systems</a></li>
+                <li><a href="#">Stations</a></li>
+                <li><a href="#">Attractions</a></li>
+                <li><a href="#">Commodities</a></li>
+                <li><a href="#">Factions</a></li>
+                <li><a href="#">Bodies</a></li>
+                <li><a href="#">Trading</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="hero">
+            <h2>Stations</h2>
+</body>
+</html>
