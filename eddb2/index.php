283f082ed6584132e48c5ecacf04c227a1e08337
llamaair <83296140+llamaair@users.noreply.github.com>
2023-04-05 17:11:43 +0200

    started working on homepage

--- eddb2/index.php
+++ eddb2/index.php
@@ -23,5 +23,17 @@
     <main>
         <section class="hero">
             <h2>Elite Dangerous Database</h2>
+            <style>
+              h2 {
+                text-align: center;
+                font-size: 50px;
+                color: blue;
+            }
+            </style>
+            <div class="description-box">
+                <p>EDDB is a website that tries to act as a database of all the data available in the game Elite Dangerous. In general, EDDB tries to help finding stuff which players are looking for. The data which can be found here is 100% community generated. There is no regular data dump from Frontier. Systems, Stations, Commoditiy Prices, Outfitting or Shipyard information are all coming from the actual player base.</p>
+            </div>
+        </section>
+    </main>
 </body>
 </html>
