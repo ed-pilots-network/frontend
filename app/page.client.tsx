5d8e4f3ef2c9b8a2368c7a8e4903974fda0e8336
aslink87 <adam.slinkman@gmail.com>
2023-06-24 15:45:49 -0400

    fix: rm duplicate tests from merge, index page heading styling

--- app/page.client.tsx
+++ app/page.client.tsx
@@ -6,7 +6,11 @@ import ModuleLaunchPad from './_components/module-launch-pad/ModuleLaunchPad';
 const PageClient = () => (
   <Center width="100%">
     <Flex flexDirection="column" gap="24px" width="100%" maxWidth="1500px">
-      <Heading as="h1" size="lg">
+      <Heading
+        as="h1"
+        size={{ base: 'md', md: 'lg', lg: 'lg' }}
+        marginX={{ base: 'auto', md: '0', lg: '0' }}
+      >
         Elite Dangerous Pilots Network
       </Heading>
       <ModuleLaunchPad />
