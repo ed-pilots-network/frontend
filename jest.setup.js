089d29333a44a398fcb306a5c0dcceb8d8914c6c
aslink87 <adam.slinkman@gmail.com>
2023-06-24 15:29:44 -0400

    test: disabled jest global matches

--- jest.setup.js
+++ jest.setup.js
@@ -3,4 +3,13 @@
 
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom/extend-expect";
+import '@testing-library/jest-dom/extend-expect';
+
+// Mock matchMedia
+global.matchMedia = () => ({
+  matches: false,
+  // eslint-disable-next-line no-undef
+  addListener: jest.fn(),
+  // eslint-disable-next-line no-undef
+  removeListener: jest.fn(),
+});
