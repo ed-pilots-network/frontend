54e67fb9dd5e2db8b56245e2676562435226ab97
nitishdhar <dhar.nitish@gmail.com>
2023-07-19 20:47:00 -0700

    Adds middleware and routes to json-server

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -4,7 +4,7 @@ import PageClient from './page.client';
 export const metadata: Metadata = {
   title: 'EDPN FAQ',
   description: 'Elite Dangerous Pilots Network',
-  icons: 'EDPN_logo.png',
+  icons: 'EDPN_logo_black.png',
 };
 
 export default function Page() {
