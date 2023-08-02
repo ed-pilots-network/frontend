ff3958b6eaee613b14cc814d9bdd3cc67cf27a7d
Chris Chrisostomou <dev@mou.me>
2023-08-02 01:23:44 +0100

    updated all tests

--- app/_components/inputs/tests/FactionStates.test.tsx
+++ app/_components/inputs/tests/FactionStates.test.tsx
@@ -1,14 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import AllegiancesField from '../Allegiances';
 import { useForm } from 'react-hook-form';
 import { SystemForm } from '../../systems/types';
+import FactionStatesField from '../FactionStates';
 
-describe('AllegiancesField', () => {
+describe('FactionStatesField', () => {
   beforeEach(() => {
     const Component = () => {
       const { register } = useForm<SystemForm>();
 
-      return <AllegiancesField register={register('allegiance')} />;
+      return <FactionStatesField register={register('economy')} />;
     };
 
     render(<Component />);
@@ -21,6 +21,6 @@ describe('AllegiancesField', () => {
   });
 
   it('includes the correct number of options', () => {
-    expect(screen.getAllByRole('option').length).toBe(9);
+    expect(screen.getAllByRole('option').length).toBe(31);
   });
 });
