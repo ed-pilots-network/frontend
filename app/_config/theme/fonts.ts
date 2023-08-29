9d025d569c7f06fbc302847bb1db6c06f548243b
Nitish Dhar <dhar.nitish@gmail.com>
2023-08-29 12:53:21 -0700

    Sets default font weight & fixes font loading in local dev (#95)

--- app/_config/theme/fonts.ts
+++ app/_config/theme/fonts.ts
@@ -2,11 +2,13 @@ import localFont from 'next/font/local';
 import { Orbitron, Rubik } from 'next/font/google';
 
 export const orbitron = Orbitron({
+  weight: ['400'],
   subsets: ['latin'],
   display: 'swap',
 });
 
 export const rubik = Rubik({
+  weight: ['400'],
   subsets: ['latin'],
   display: 'swap',
 });
