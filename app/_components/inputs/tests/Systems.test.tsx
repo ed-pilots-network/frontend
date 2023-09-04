51e6d85ffeeb5c56ce829020bd2d2f6a0eadba97
Chris Chrisostomou <dev@mou.me>
2023-09-04 13:18:49 +0100

    test

---
+++ app/_components/inputs/tests/Systems.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import { useForm } from 'react-hook-form';
+import { SystemForm } from '@/app/_types/system';
+import SystemsField from '../Systems';
+import { ChakraProvider } from '@chakra-ui/react';
+
+describe('Systems Field', () => {
+  beforeEach(() => {
+    const Component = () => {
+      const { control } = useForm<SystemForm>();
+
+      return (
+        <ChakraProvider>
+          <SystemsField fieldName="testField" control={control} />
+        </ChakraProvider>
+      );
+    };
+
+    render(<Component />);
+  });
+
+  it('renders', () => {
+    expect(screen.getByRole('combobox'));
+  });
+});
