8d3e6e043e42a160206608bcbf9b5fdf20ff0d50
aslink87 <adam.slinkman@gmail.com>
2023-09-12 21:54:11 -0400

    fix test

--- app/template/page.test.tsx
+++ app/template/page.test.tsx
@@ -5,5 +5,5 @@ test('Template Page - renders the heading with correct text', () => {
   render(<PageClient />);
   const headingElement = screen.getByRole('heading', { level: 1 });
   expect(headingElement).toBeInTheDocument();
-  expect(headingElement.textContent).toBe('Template Page');
+  expect(headingElement.textContent).toBe('Heading goes here');
 });
