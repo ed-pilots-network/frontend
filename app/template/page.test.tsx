578061f5c876c43b35177f625aa29aecbb5ba2da
aslink87 <adam.slinkman@gmail.com>
2023-09-13 20:47:25 -0400

    fix unit test

--- app/template/page.test.tsx
+++ app/template/page.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import PageClient from './page.client';
 
 test('Template Page - renders the heading with correct text', () => {
-  render(<PageClient />);
+  render(<PageClient serverData={[]} />);
   const headingElement = screen.getByRole('heading', { level: 1 });
   expect(headingElement).toBeInTheDocument();
   expect(headingElement.textContent).toBe('Heading goes here');
