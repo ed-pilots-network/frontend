9cdf53f8520f4d8bb657c6488f7ccd6bd3e75c97
Adam Slinkman <adam.slinkman@gmail.com>
2023-09-14 10:30:09 -0400

    flattened remaining mock data

--- mock/exploration/station/filter.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-  { name: 'Daedalus' },
-  { name: 'Ehrlich City' },
-  { name: 'Abraham Lincoln' },
-  { name: 'Mars High' },
-  { name: 'Titan City' },
-  { name: "Dekker's Yard" },
-  { name: 'Durrance Camp' },
-];
