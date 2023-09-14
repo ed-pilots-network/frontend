33a2bda20dfcaf3472cb5d4c91ac1ec5106f48f1
Nitish Dhar <dhar.nitish@gmail.com>
2023-09-14 07:04:39 -0700

    Implements nesting with flat data setup

--- mock/_db.js
+++ mock/_db.js
@@ -1,15 +1,19 @@
 const Path = require('path');
 const glob = require('glob');
 
-const apiFiles = glob.sync(Path.join(__dirname, './**/[!_]*.js'));
+const apiFiles = glob.sync(Path.join(__dirname, './[!_]*.js'));
 
 let data = {};
 
 apiFiles.forEach((filePath) => {
   const api = require(filePath);
   let [, url] = filePath.split('mock/');
-  url = url.slice(0, url.length - 3); // remove .js
-  data[url.replace(/\//g, '-')] = api;
-});
 
+  url = url.replace('.js', ''); // remove .js
+  for (let key in api) {
+    if (api.hasOwnProperty(key)) {
+      data[key] = api[key];
+    }
+  }
+});
 module.exports = () => data;
