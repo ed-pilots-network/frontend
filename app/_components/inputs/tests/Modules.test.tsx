5cedf9579315cf56b0de38ee26f1c04839981734
Chris Chrisostomou <dev@mou.me>
2023-09-17 23:33:10 +0100

    updated tests

--- app/_components/inputs/tests/Modules.test.tsx
+++ app/_components/inputs/tests/Modules.test.tsx
@@ -2,8 +2,8 @@ import { act, render, screen } from '@testing-library/react';
 import { useForm } from 'react-hook-form';
 import { ChakraProvider, FormControl, FormLabel } from '@chakra-ui/react';
 import selectEvent from 'react-select-event';
-import { StationForm } from '@/app/_types/forms';
 import ModulesField from '../Modules';
+import { StationForm } from '@/app/_types/station';
 
 const Component = ({ isMulti = false }: { isMulti?: boolean }) => {
   const { control } = useForm<StationForm>();
@@ -17,38 +17,7 @@ const Component = ({ isMulti = false }: { isMulti?: boolean }) => {
 
 const MODULES = [
   '1I Detailed Surface Scanner',
-  '5D Fighter Hangar',
-  '6D Fighter Hangar',
-  '7D Fighter Hangar',
-  '2A Frame Shift Drive',
-  '2B Frame Shift Drive',
-  '2C Frame Shift Drive',
-  '2D Frame Shift Drive',
   '2E Frame Shift Drive',
-  '3A Frame Shift Drive',
-  '3B Frame Shift Drive',
-  '3C Frame Shift Drive',
-  '3D Frame Shift Drive',
-  '3E Frame Shift Drive',
-  '4A Frame Shift Drive',
-  '4B Frame Shift Drive',
-  '4C Frame Shift Drive',
-  '4D Frame Shift Drive',
-  '4E Frame Shift Drive',
-  '5A Frame Shift Drive',
-  '5B Frame Shift Drive',
-  '5C Frame Shift Drive',
-  '5D Frame Shift Drive',
-  '5E Frame Shift Drive',
-  '6A Frame Shift Drive',
-  '6B Frame Shift Drive',
-  '6C Frame Shift Drive',
-  '6D Frame Shift Drive',
-  '6E Frame Shift Drive',
-  '0A Kill Warrant Scanner',
-  '0B Kill Warrant Scanner',
-  '0C Kill Warrant Scanner',
-  '0D Kill Warrant Scanner',
   '0E Kill Warrant Scanner',
   '1E Supercruise Assist',
 ];
--- app/_components/inputs/tests/Ships.test.tsx
+++ app/_components/inputs/tests/Ships.test.tsx
@@ -15,46 +15,7 @@ const Component = ({ isMulti = false }: { isMulti?: boolean }) => {
   );
 };
 
-const SHIPS = [
-  'Adder',
-  'Alliance Challenger',
-  'Alliance Chieftain',
-  'Alliance Crusader',
-  'Anaconda',
-  'Asp Explorer',
-  'Asp Scout',
-  'Beluga Liner',
-  'Cobra MkIII',
-  'Cobra MkIV',
-  'Diamondback Explorer',
-  'Diamondback Scout',
-  'Dolphin',
-  'Eagle MkII',
-  'Federal Assault Ship',
-  'Federal Corvette',
-  'Federal Dropship',
-  'Federal Gunship',
-  'Fer-de-Lance',
-  'Hauler',
-  'Imperial Clipper',
-  'Imperial Courier',
-  'Imperial Cutter',
-  'Imperial Eagle',
-  'Keelback',
-  'Krait MkII',
-  'Krait Phantom',
-  'Mamba',
-  'Orca',
-  'Python',
-  'Sidewinder MkI',
-  'Type-6 Transporter',
-  'Type-7 Transporter',
-  'Type-9 Heavy',
-  'Type-10 Defender',
-  'Viper MkIII',
-  'Viper MkIV',
-  'Vulture',
-];
+const SHIPS = ['Anaconda', 'Fer-de-Lance', 'Vulture'];
 
 describe('Ships Field', () => {
   it('renders', () => {
