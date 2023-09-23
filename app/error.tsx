69717408c7a83e3ac337f00822358f40c4022b25
Adam Slinkman <adam.slinkman@gmail.com>
2023-09-23 15:04:50 -0400

    hide error message in prod

--- app/error.tsx
+++ app/error.tsx
@@ -25,7 +25,9 @@ export default function Error({
         >
           Something went wrong!
         </Heading>
-        <Text as="samp">{error.message}</Text>
+        <Text as="samp">
+          {process.env.NODE_ENV === 'development' && error.message}
+        </Text>
         <Button type="button" variant="outline" onClick={() => reset()}>
           Try again
         </Button>
