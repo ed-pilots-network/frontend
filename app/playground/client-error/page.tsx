37175a26357230bcf3d62b799c56af9a96ead5cd
Adam Slinkman <adam.slinkman@gmail.com>
2023-09-23 15:46:46 -0400

    update fetch to match template, removed custom error pages

--- app/playground/client-error/page.tsx
+++
@@ -1,8 +0,0 @@
-import PageClient from './page.client';
-
-// Avoid statically building this page during build.
-export const dynamic = 'force-dynamic';
-
-export default async function Page() {
-  return <PageClient />;
-}
